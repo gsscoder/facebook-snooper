cfb54125d22696929c310b243ecdbdf4c6315362
Giacomo Stelluti Scala <gsscoder@gmail.com>
2019-11-24 11:25:18 +0100

    FacebookSession merged to Session

--- facebook_snooper/__init__.py
+++ facebook_snooper/__init__.py
@@ -1,7 +1,6 @@
 from .__version__ import __version__
 from .core.session import Session
-from .core.session import FacebookSession
 
 
 def default_session():
-    return FacebookSession()
+    return Session()
--- facebook_snooper/core/session.py
+++ facebook_snooper/core/session.py
@@ -7,12 +7,13 @@ from ._parser import Parser
 
 
 __all__ = [
-    "Session",
-    "FacebookSession"
+    "Session"
 ]
 
 
 class Session:
+    BASE_URL = 'https://m.facebook.com'
+
     def __init__(self):
         self._connected = False
         self._current_html = None
@@ -40,14 +41,33 @@ class Session:
     def connected(self):
         return self._connected
 
-    @abstractmethod
     def log_in(self, username, password):
-        """Log in to Facebook with username and password."""
-        pass
+        try:
+            self._browser = StatefulBrowser()
+            self._browser.addHeaders = [
+                    ('User-Agent', 'Mozilla/5.0 (Linux; Android 7.0; ' + \
+                        'PLUS Build/NRD90M) AppleWebKit/537.36 (KHTML, ' + \
+                        'like Gecko) Chrome/61.0.3163.98 Mobile ' + \
+                        'Safari/537.36'), \
+                    ('Accept-Language', 'en-US,en;q=0.5')
+                    ]
+            self._browser.open(Session.BASE_URL)
+            self._current_html = str(self._browser.get_current_page())
+            self._browser.select_form('form[id="login_form"]')
+            self._browser['email'] = username
+            self._browser['pass'] =  password        
+            self._browser.submit_selected()
+            self._connected = True
+        except:
+            raise LogInError(f'Unable to log in as {username}')
+        return self
 
-    @abstractmethod
     def log_out(self):
-        pass
+        if self._connected:
+            self._browser.close()
+            self._browser = None
+            self._connected = False
+
 
     def profile_info(self, id_):
         """Retrieve informations for a given profile."""
@@ -69,21 +89,23 @@ class Session:
         except:
             return None
 
-    @abstractmethod
     def _get_current_title(self):
-        pass
+        return self._browser.get_current_page().find('title').text
 
-    @abstractmethod
     def _get_profile_html(self, profile_id):
-        pass
+        url = f'{Session.BASE_URL}/{profile_id}'
+        self._browser.open(url)
+        self._current_html = str(self._browser.get_current_page())
+        return self._current_html
 
-    @abstractmethod
     def _get_search_soup(self, query):
-        pass
+        url_query = '+'.join(query.split())
+        self._browser.open(f'{Session.BASE_URL}/search/top/?q={url_query}')
+        self._current_html = str(self._browser.get_current_page())
+        return self._browser.get_current_page()
 
-    @abstractmethod
     def _get_current_soup(self):
-        pass
+        return self._browser.get_current_page()
 
     def _ensure_connected(self):
         if not self._connected:
@@ -94,51 +116,3 @@ class Session:
         if '-' in title:
             return title.split('-')[0].strip()
         return title
-
-
-class FacebookSession(Session):
-    def log_in(self, username, password):
-        self._base_url = 'https://m.facebook.com'
-        try:
-            self._browser = StatefulBrowser()
-            self._browser.addHeaders = [
-                    ('User-Agent', 'Mozilla/5.0 (Linux; Android 7.0; ' + \
-                        'PLUS Build/NRD90M) AppleWebKit/537.36 (KHTML, ' + \
-                        'like Gecko) Chrome/61.0.3163.98 Mobile ' + \
-                        'Safari/537.36'), \
-                    ('Accept-Language', 'en-US,en;q=0.5')
-                    ]
-            self._browser.open(self._base_url)
-            self._current_html = str(self._browser.get_current_page())
-            self._browser.select_form('form[id="login_form"]')
-            self._browser['email'] = username
-            self._browser['pass'] =  password        
-            self._browser.submit_selected()
-            self._connected = True
-        except:
-            raise LogInError(f'Unable to log in as {username}')
-        return self
-
-    def log_out(self):
-        if self._connected:
-            self._browser.close()
-            self._browser = None
-            self._connected = False
-
-    def _get_current_title(self):
-        return self._browser.get_current_page().find('title').text
-
-    def _get_profile_html(self, profile_id):
-        url = f'{self._base_url}/{profile_id}'
-        self._browser.open(url)
-        self._current_html = str(self._browser.get_current_page())
-        return self._current_html
-
-    def _get_search_soup(self, query):
-        url_query = '+'.join(query.split())
-        self._browser.open(f'{self._base_url}/search/top/?q={url_query}')
-        self._current_html = str(self._browser.get_current_page())
-        return self._browser.get_current_page()
-
-    def _get_current_soup(self):
-        return self._browser.get_current_page()
