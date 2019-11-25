6911eeae222b93d9b28f1b88b8980902793b8f06
Giacomo Stelluti Scala <gsscoder@gmail.com>
2019-11-25 09:50:51 +0100

    Added __all__ variable to persistence.py in tests

--- tests/persistence.py
+++ tests/persistence.py
@@ -4,6 +4,12 @@ sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__),
     '..')))
 from facebook_snooper import Session
 
+
+__all__ = [
+    "save_current_page"
+]
+
+
 def save_current_page(session: Session, filename):
     html_ = str(session._browser.get_current_page())
     with open(f'./tests/pages/{filename}.html', 'w') as page:
