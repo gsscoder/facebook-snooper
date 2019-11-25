55e21d3f9828f52849d05b8d2017131a9956870b
Giacomo Stelluti Scala <gsscoder@gmail.com>
2019-11-25 08:47:35 +0100

    Added persistence module to tests

---
+++ tests/persistence.py
@@ -0,0 +1,10 @@
+import os
+import sys
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__),
+    '..')))
+from facebook_snooper import Session
+
+def save_current_page(session: Session, filename):
+    html_ = str(session._browser.get_current_page())
+    with open(f'./tests/pages/{filename}.html', 'w') as page:
+        page.write(html_)
